62232d7896da9948da2ab0646d3068d9cda304c1
karan sivarat <bomb0069@gmail.com>
2021-08-28 15:13:53 +0700

    [Added]: Basket and Item

---
+++ src/main/java/demo/Basket.java
@@ -0,0 +1,44 @@
+package demo;
+
+import java.util.List;
+
+public class Basket {
+
+    int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public double getNetTotal() {
+        return netTotal;
+    }
+
+    public void setNetTotal(double netTotal) {
+        this.netTotal = netTotal;
+    }
+
+    List<Item> items;
+    double discount;
+    double netTotal;
+}
---
+++ src/main/java/demo/Item.java
@@ -0,0 +1,23 @@
+package demo;
+
+public class Item {
+    Book book;
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    int quantity;
+}
